553a6bb1438cab61560b1e55a521ea659aa0a5d4
Claus Dahl <dee@classy.dk>
2014-10-23 13:40:43 +0200

    better assertions. It does work

--- test/test_matrix.rb
+++ test/test_matrix.rb
@@ -8,9 +8,10 @@ class MatrixTest < Test::Unit::TestCase
   def test_product
     m = ::Autodiff::Matrix.new
     n = ::Autodiff::Matrix.new
-    m*n
-    m.set(Matrix[[1,2],[3,4]])
+    product = m*n
+    m.set(Matrix[[1,0],[0,2]])
     n.set(Matrix[[1,2],[3,4]])
+    assert_equal product.value, Matrix[[1,2],[6,8]]
   end
 
   def test_gradient
@@ -19,11 +20,11 @@ class MatrixTest < Test::Unit::TestCase
     v1.set(::Matrix[[1,2,3,4,5]])
     #p dotproduct.value.to_a
     dotproduct.accumulate(Matrix[[1]])
-    p v1.gradient
+    assert_equal v1.gradient, 2*::Matrix[[1,2,3,4,5]]
   end
 
   def test_norm
-
+    
   end
 
 end
